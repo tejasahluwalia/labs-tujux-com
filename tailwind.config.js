a8be152965101732453b949ade437a49b5da51fc
Tejas Ahluwalia <tejasahluwalia@gmail.com>
2024-07-23 21:45:59 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}", "./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        mono:
+          "'Nimbus Mono PS', 'ui - monospace', 'SFMono - Regular', Menlo, Monaco, Consolas, 'Liberation Mono', 'Courier New', monospace",
+      },
+    },
+  },
+  plugins: [],
+};
